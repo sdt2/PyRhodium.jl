1cac2fa5a5c81525d14db8bbdd1dbdd47c3a75af
David Anthoff <anthoff@berkeley.edu>
2017-06-05 17:38:08 -0700

    Add build.jl

---
+++ deps/build.jl
@@ -0,0 +1,13 @@
+using PyCall
+using Conda
+
+Conda.add("pip")
+Conda.add("seaborn")
+Conda.add("scikit-learn")
+
+pip = joinpath(Conda.SCRIPTDIR, "pip")
+run(`$pip install --no-deps pydotplus`)
+run(`$pip install --no-deps SAlib`)
+run(`$pip install --no-deps git+https://github.com/Project-Platypus/PRIM.git\#egg=prim`)
+run(`$pip install --no-deps git+https://github.com/Project-Platypus/Platypus.git\#egg=platypus`)
+run(`$pip install --no-deps git+https://github.com/Project-Platypus/Rhodium.git\#egg=rhodium`)
