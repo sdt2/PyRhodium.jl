0c58b095368cdb1d9a48b0db5a70e0c73d1058d7
David Anthoff <anthoff@berkeley.edu>
2017-06-06 14:08:19 -0700

    Simplify lake example

--- examples/lake.jl
+++ examples/lake.jl
@@ -4,37 +4,40 @@ using Distributions
 
 
 function lake_problem(;pollution_limit=nothing,
-         b = 0.42,        # decay rate for P in lake (0.42 = irreversible)
-         q = 2.0,         # recycling exponent
-         mean = 0.02,     # mean of natural inflows
-         stdev = 0.001,   # standard deviation of natural inflows
-         alpha = 0.4,     # utility from pollution
-         delta = 0.98,    # future utility discount rate
-         nsamples = 100)  # monte carlo sampling of natural inflows)
+         b = 0.42,       # decay rate for P in lake (0.42 = irreversible)
+         q = 2.0,        # recycling exponent
+         μ = 0.02,       # mean of natural inflows
+         σ = 0.001,      # standard deviation of natural inflows
+         α = 0.4,        # utility from pollution
+         δ = 0.98,       # future utility discount rate
+         nsamples = 100) # monte carlo sampling of natural inflows)
+
     Pcrit = fzero(x -> x^q/(1+x^q) - b*x, 0.01, 1.5)
     nvars = length(pollution_limit)
     X = zeros(nvars)
     average_daily_P = zeros(nvars)
     reliability = 0.0
 
+    d = LogNormal(log(μ^2 / sqrt(σ^2 + μ^2)),sqrt(log(1.0 + σ^2 / μ^2)))
+    
+    natural_inflows = zeros(nvars)
+    
     for i in 1:nsamples
-        X[1] = 0.0
-
-        d = LogNormal(log(mean^2 / sqrt(stdev^2 + mean^2)),sqrt(log(1.0 + stdev^2 / mean^2)))
+        X[1] = 0.0        
         
-        natural_inflows = rand(d, nvars)
+        rand!(d, natural_inflows)
         
         for t in 2:nvars
-            X[t] = (1-b)*X[t-1] + X[t-1]^q/(1+X[t-1]^q) + pollution_limit[t-1] + natural_inflows[t-1]
-            average_daily_P[t] += X[t]/float(nsamples)
+            X[t] = (1-b)*X[t-1] + X[t-1]^q/(1+X[t-1]^q) + pollution_limit[t] + natural_inflows[t]
+            average_daily_P[t] += X[t]/nsamples
         end
     
         reliability += sum(X .< Pcrit)/(nsamples*nvars)
     end
       
     max_P = maximum(average_daily_P)
-    utility = sum(alpha*pollution_limit.*delta.^collect(1:nvars))
-    intertia = sum(diff(pollution_limit) .> -0.02)/float(nvars-1)
+    utility = sum(α.*pollution_limit.*δ.^collect(1:nvars))
+    intertia = sum(diff(pollution_limit) .> -0.02)/(nvars-1)
     
     return max_P, utility, intertia, reliability
 end
@@ -55,8 +58,8 @@ setresponses(m, [Response("max_P", :MINIMIZE),
 
 setlevers(m, [RealLever("pollution_limit", 0.0, 0.1, length=100)])
 
-output = optimize(m, "NSGAII", 10000)
+output = optimize(m, "NSGAII", 100)
 
 println("Found $(length(output)) optimal policies!")
 
-fig = scatter2d(output)
+# fig = scatter2d(output)
